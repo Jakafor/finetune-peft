119add7c8ac29805a62ffd296a1d325a586d1b85
Jakafor <jkforstner@gmail.com>
2025-09-22 15:29:29 +0200

    adapted import

---
+++ models/sam/modeling/prompt_encoder_adapter.py
@@ -0,0 +1,18 @@
+# copyright ziqi-jin
+
+import torch.nn as nn
+from .sam import Sam
+
+
+
+class BasePromptEncodeAdapter(nn.Module):
+
+    def __init__(self, ori_sam: Sam, fix=False):
+        super(BasePromptEncodeAdapter, self).__init__()
+
+        self.sam_prompt_encoder = ori_sam.prompt_encoder
+        
+
+    def forward(self, points=None, boxes=None, masks=None):
+        sparse_embeddings, dense_embeddings = self.sam_prompt_encoder(points, boxes, masks)
+        return sparse_embeddings, dense_embeddings
