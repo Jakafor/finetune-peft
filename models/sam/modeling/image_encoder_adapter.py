6d1ef5fae937b762e903d4f0e972750f2ead1d2a
Jakafor <jkforstner@gmail.com>
2025-09-22 15:24:46 +0200

    remove fix parameters

---
+++ models/sam/modeling/image_encoder_adapter.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+from sam import Sam
+
+
+
+class BaseImgEncodeAdapter(nn.Module):
+
+    def __init__(self, ori_sam: Sam, fix=False):
+        super(BaseImgEncodeAdapter, self).__init__()
+        self.sam_img_encoder = ori_sam.image_encoder
+        
+
+    def forward(self, x):
+        x = self.sam_img_encoder(x)
+        return x
